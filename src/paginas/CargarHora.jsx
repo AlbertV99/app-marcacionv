9e8b59165cc41dc5ea5ef5b32646c8c17b18c785
AlbertV99 <tico.urquhart@gmail.com>
2023-04-04 20:06:49 -0400

    Se agrega posibilidad de quitar foto

--- src/paginas/CargarHora.jsx
+++ src/paginas/CargarHora.jsx
@@ -4,37 +4,52 @@ import {Container,Navbar,Row,Col,Button,Form} from 'react-bootstrap';
 import MenuInferior from '../components/menuInf'
 import Webcam from "react-webcam";
 const CargarHora = (props) => {
+    const webcamRef = React.useRef(null);
+    const [imgSrc, setImgSrc] = React.useState(null);
+    const capture = React.useCallback(
+        () => {
+            const imageSrc = webcamRef.current.getScreenshot();
+            setImgSrc(imageSrc);
+        },
+        [webcamRef,setImgSrc]
+    )
     const videoConstraints = {
-      facingMode: "left"
+        facingMode: "user"
     };
 
     return (
         <>
-            <Form>
-                <Container fluid>
-                    <Row>
-                        <Col>
-                            <h2>Registro de Horas</h2>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <Webcam audio={false} height={200} videoConstraints={videoConstraints} width={200}></Webcam>
-                        </Col>
-                    </Row>
-                </Container>
-                <Navbar fixed='bottom' style={{position:'fixed',bottom:"100px",width:"100%"}}>
-                    <Button variant="primary">
-                        Entrada
-                    </Button>
-                    <Button variant="primary">
-                        Salida
-                    </Button>
-                </Navbar>
-            </Form>
+        <Form>
+            <Container fluid>
+                <Row>
+                    <Col>
+                        <h2>Registro de Horas</h2>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Webcam audio={false} height={200}ref={webcamRef} screenshotFormat="image/jpeg" videoConstraints={videoConstraints} width={200}></Webcam>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        {imgSrc && (
+                            <img
+                              src={imgSrc}
+                            />
+                          )}
+                    </Col>
+                </Row>
+            </Container>
+            <Navbar fixed='bottom' style={{position:'fixed',bottom:"100px",width:"100%"}}>
+                <Button variant="primary" onClick={capture}>
+                    Marcar
+                </Button>
+            </Navbar>
+        </Form>
 
         </>
-    )
+)
 }
 
 export default CargarHora
