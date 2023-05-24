33b061752f2e418b23b8589a969d0ffe69024310
AlbertV99 <tico.urquhart@gmail.com>
2023-05-24 00:48:38 -0400

    Se crean componentes para aplicacion de multiples usuarios. Se modifica el main para utilizar los nuevos componentes. Se modifica el menu inferior para las opciones de ajustes y marcar nada mas. Se crea el componente multiple Registro para poder registrar a los usuarios y realizar la marca.

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter,Routes,Route } from "react-router-dom"
 import App from './App'
-import Registro from './paginas/Registro'
+// import Registro from './paginas/Registro'
+import MultipleRegistro from './paginas/MultipleRegistro'
 import CargarHora from './paginas/CargarHora'
 import Historial from './paginas/Historial'
 import './index.css'
@@ -11,8 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<App/>}>
-                <Route index element={<Registro />} />
-                <Route  path='config' element={<Registro/>} />
+                <Route index element={<MultipleRegistro />} />
+                {/*<Route  path='config' element={<Registro/>} /> */}
+                <Route  path='config' element={<MultipleRegistro/>} />
                 <Route  path='historial' element={<Historial/>} />
                 <Route  path='cargarHora' element={<CargarHora/>} />
             </Route>
