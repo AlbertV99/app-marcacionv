45a7e2dae6adcf4901db5a9e4112f5f2c23f441f
AlbertV99 <tico.urquhart@gmail.com>
2023-11-02 16:48:14 -0300

    Actualizacion de version apuntando a produccion

--- src/helpers/constantes.js
+++ src/helpers/constantes.js
@@ -1,13 +1,11 @@
 const Constantes = ()=>{
     // const HOST = "http://localhost/marcacion";
     // const HOST = "http://192.168.100.88/marcacion";
-    const HOST = "https://vmarcador.valurq.com"
-
-
+    // const HOST = "https://vmarcador.valurq.com"
+    const HOST = "https://marcador.valurq.com"
 
     return {
         "host":HOST,
-
     };
 }
 export default Constantes
